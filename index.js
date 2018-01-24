5a1372bf9d432ae885a27aabb3850e9e7de7d77d
Tyler Reardon <tdr.reardon@gmail.com>
2018-01-23 19:18:20 -0800

    WINDOW2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const window = window || { location: {pathname: ''}, history: [] } // eslint-disable-line
+let window // eslint-disable-line
+if (typeof window === 'undefined') {
+  window = { location: {pathname: ''}, history: [] }
+}
+
 require('./dist/manifest');
 require('./dist/vendor');
 module.exports = require('./dist/index').default;
