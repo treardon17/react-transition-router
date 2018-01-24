3df45bcc6e275e08eab11cccbbeb3862e5c34a0f
Tyler Reardon <tdr.reardon@gmail.com>
2018-01-23 19:08:35 -0800

    Even more window stuff

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('window')();
+const Window = require('window');
+const window = new Window();
 require('./dist/manifest');
 require('./dist/vendor');
 module.exports = require('./dist/index').default;
